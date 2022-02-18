cb7748decb0c27725f4cec41d9f592d48817ce1c
druid <angular.druid@gmail.com>
2022-02-17 23:09:41 -0500

    Fix file suffix.

--- bin/deploy_prod.sh
+++ bin/deploy_prod.sh
@@ -1,4 +1,4 @@
 ng run ipe:build --deployUrl="https://dpxmq1mxvsyda.cloudfront.net/"
 ng run ipe:server --deployUrl="https://dpxmq1mxvsyda.cloudfront.net/"
 ng run ipe:lambda:dev --deployUrl="https://dpxmq1mxvsyda.cloudfront.net/"
-serverless deploy --aws-profile prod 
+serverless deploy --aws-profile prod
