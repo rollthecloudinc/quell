c179c65ab83fb7ef89e2a908d3e4963c0ce447ad
druid <angular.druid@gmail.com>
2022-03-09 01:17:18 -0500

    Resolve module context by loading contexts federated module.

--- modules/tractorbeam/src/lib/resolvers/module.resolver.ts
+++ modules/tractorbeam/src/lib/resolvers/module.resolver.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ContextPlugin, ContextResolver } from 'context';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 @Injectable()
 export class ModuleResolver implements ContextResolver {
@@ -8,6 +9,17 @@ export class ModuleResolver implements ContextResolver {
   constructor() { }
 
   resolve(ctx: ContextPlugin, data?: any, metadata?: Map<string, any>): Observable<any> {
-    return of(true);
+    console.log('module resolver context', ctx, data, metadata);
+    return new Observable<undefined>(obs => {
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: data.remoteEntry,
+        exposedModule: data.exposedModule
+      }).then(() => {
+        console.log('module resolver loaded', ctx.name);
+        obs.next();
+        obs.complete();
+      });
+    });
   }
 }
