656398143b893c2025be747c84215454f924fbe5
druid <angular.druid@gmail.com>
2022-06-04 18:56:26 -0400

    Move aws rum monitor to separate thread to prevent blocking page load.

--- modules/awrum/src/lib/awrum.factories.ts
+++ modules/awrum/src/lib/awrum.factories.ts
@@ -4,30 +4,32 @@ import { CloudwatchRumSettings } from './models/rum.models';
 
 export const initializeRumMonitorFactory = (rumSettings: CloudwatchRumSettings): () => Observable<any> => {
   return () => new Observable(obs => {
-    try {
-      const config: AwsRumConfig = {
-        sessionSampleRate: 1,
-        identityPoolId: rumSettings.identityPoolId,
-        guestRoleArn: rumSettings.guestRoleArn,
-        endpoint: "https://dataplane.rum." + rumSettings.region + ".amazonaws.com",
-        telemetries: ["performance","errors","http"],
-        allowCookies: true,
-        enableXRay: true
-      };
-    
-      const APPLICATION_ID: string = rumSettings.appId;
-      const APPLICATION_VERSION: string = '1.0.0';
-      const APPLICATION_REGION = rumSettings.region;
-    
-      const awsRum: AwsRum = new AwsRum(
-        APPLICATION_ID,
-        APPLICATION_VERSION,
-        APPLICATION_REGION,
-        config
-      );
-    } catch (error) {
-      // Ignore errors thrown during CloudWatch RUM web client initialization
-    }
+    setTimeout(() => {
+      try {
+        const config: AwsRumConfig = {
+          sessionSampleRate: 1,
+          identityPoolId: rumSettings.identityPoolId,
+          guestRoleArn: rumSettings.guestRoleArn,
+          endpoint: "https://dataplane.rum." + rumSettings.region + ".amazonaws.com",
+          telemetries: ["performance","errors","http"],
+          allowCookies: true,
+          enableXRay: true
+        };
+      
+        const APPLICATION_ID: string = rumSettings.appId;
+        const APPLICATION_VERSION: string = '1.0.0';
+        const APPLICATION_REGION = rumSettings.region;
+      
+        const awsRum: AwsRum = new AwsRum(
+          APPLICATION_ID,
+          APPLICATION_VERSION,
+          APPLICATION_REGION,
+          config
+        );
+      } catch (error) {
+        // Ignore errors thrown during CloudWatch RUM web client initialization
+      }
+    });
     obs.next();
     obs.complete();
   });
