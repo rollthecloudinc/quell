e5965a7159b2e601bbb02ab2d4f453736248bc7c
druid <angular.druid@gmail.com>
2022-06-30 00:11:01 -0400

    Replace prod lib build using npm run instead of ng.

--- bin/build_libs_prod.sh
+++ bin/build_libs_prod.sh
@@ -1,42 +1,42 @@
-ng run utils:build:production
-ng run sheath:build:production
-ng run auth:build:production
-ng run oidc:build:production
-ng run plugin:build:production
-ng run material:build:production
-ng run attributes:build:production
-ng run content:build:production
-ng run alias:build:production
-ng run token:build:production
-ng run snippet:build:production
-ng run bridge:build:production
-ng run dparam:build:production
-ng run durl:build:production
-ng run datasource:build:production
-ng run context:build:production
-ng run rules:build:production
-ng run refinery:build:production
-ng run crud:build:production
-ng run alienalias:build:production
-ng run keyval:build:production
-ng run awcog:build:production
-ng run awrum:build:production
-ng run aws3:build:production
-ng run awos:build:production
-ng run deity:build:production
-ng run style:build:production
-ng run transform:build:production
-ng run react:build:production
-ng run media:build:production
-ng run panels:build:production
-ng run outsider:build:production
-ng run tractorbeam:build:production
-ng run layout:build:production
-ng run state:build:production
-ng run loop:build:production
-ng run rest:build:production
-ng run render:build:production
-ng run pages:build:production
-ng run pagealias:build:production
-ng run formly:build:production
-ng run forms:build:production
+npm run build utils -- --configuration production
+npm run build sheath -- --configuration production
+npm run build auth -- --configuration production
+npm run build oidc -- --configuration production
+npm run build plugin -- --configuration production
+npm run build material -- --configuration production
+npm run build attributes -- --configuration production
+npm run build content -- --configuration production
+npm run build alias -- --configuration production
+npm run build token -- --configuration production
+npm run build snippet -- --configuration production
+npm run build bridge -- --configuration production
+npm run build dparam -- --configuration production
+npm run build durl -- --configuration production
+npm run build datasource -- --configuration production
+npm run build context -- --configuration production
+npm run build rules -- --configuration production
+npm run build refinery -- --configuration production
+npm run build crud -- --configuration production
+npm run build alienalias -- --configuration production
+npm run build keyval -- --configuration production
+npm run build awcog -- --configuration production
+npm run build awrum -- --configuration production
+npm run build aws3 -- --configuration production
+npm run build awos -- --configuration production
+npm run build deity -- --configuration production
+npm run build style -- --configuration production
+npm run build transform -- --configuration production
+npm run build react -- --configuration production
+npm run build media -- --configuration production
+npm run build panels -- --configuration production
+npm run build outsider -- --configuration production
+npm run build tractorbeam -- --configuration production
+npm run build layout -- --configuration production
+npm run build state -- --configuration production
+npm run build loop -- --configuration production
+npm run build rest -- --configuration production
+npm run build render -- --configuration production
+npm run build pages -- --configuration production
+npm run build pagealias -- --configuration production
+npm run build formly -- --configuration production
+npm run build forms -- --configuration production
