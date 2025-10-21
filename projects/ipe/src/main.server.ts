dee9b218dcf3169827835041506eb0243772543c
tzmijewski <angular.druid@gmail.com>
2025-10-21 17:31:37 -0400

    Version bump with changes for fixing build.

--- projects/ipe/src/main.server.ts
+++ projects/ipe/src/main.server.ts
@@ -1,51 +1,38 @@
 import 'zone.js/node';
 
-export { ngExpressEngine } from '@angular/ssr'; 
-export { enableProdMode } from '@angular/core';
-export { AppServerModule } from './app/app.server.module';
-export { HOST_NAME, PROTOCOL } from '@rollthecloudinc/utils';
-export { renderModule } from '@angular/platform-server';
-// export { renderModule, renderModuleFactory } from '@angular/platform-server';
-
-// import { renderModule, renderModuleFactory } from '@angular/platform-server';
-// import { environment } from './environments/environment';
-import { ngExpressEngine } from '@angular/ssr';
-import { AppServerModule } from './app/app.server.module';
-
-// import 'localstorage-polyfill';
-import * as express from 'express';
-// const { ngExpressEngine, AppServerModule, enableProdMode } = require('../../../dist/ipe/server/main');
-import { APP_BASE_HREF } from '@angular/common';
 import { enableProdMode } from '@angular/core';
-import { REQUEST } from './express.tokens';
+import { AppServerModule } from './app/app.server.module';
 import { HOST_NAME, PROTOCOL } from '@rollthecloudinc/utils';
+import { renderModule } from '@angular/platform-server';
+
+// 🚨 FINAL FIX: Import CommonEngine from the specific '/node' entry point
+import { CommonEngine } from '@angular/ssr/node'; 
+
+import express from 'express'; // Fixed: Use 'import express from' for correct typing
+import { APP_BASE_HREF } from '@angular/common';
+import { REQUEST } from './express.tokens';
 import { join } from 'path';
-// const winston  = require('winston');
-// const  { Loggly } = require('winston-loggly-bulk');
+
 const cookieParser = require('cookie-parser');
 const proxy = require('express-http-proxy');
 
-// @todo: Required for https to function locally. Need to revisit on prod environment.
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// Create the CommonEngine instance outside the app function for reuse
+const commonEngine = new CommonEngine();
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   enableProdMode();
   const server = express();
-  const distFolder = 'dist/ipe';
-  const indexHtml = 'index';
+  const distFolder = join(process.cwd(), 'dist/spear');
+  const indexHtml = join(distFolder, 'index.html'); // Must point to the physical index.html
 
   server.use(cookieParser());
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule
-  }));
-
+  // Set up view engine for Express
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
-  // aws service proxy
+  // aws service proxy (unchanged)
   server.use('/awproxy', proxy(req => `https://${req.originalUrl.split('/')[3]}.${ req.originalUrl.split('/').length > 4 && req.originalUrl.split('/')[4].match(/^us\-(east|west)-[0-9]+$/gi) ? `${req.originalUrl.split('/')[4]}.` : '' }${req.originalUrl.split('/')[2]}.amazonaws.com` , {
     proxyReqPathResolver: req => {
       console.log(`current request path: ${req.url}`);
@@ -60,24 +47,30 @@ export function app() {
     }
   }));
 
-  // Example Express Rest API endpoints
-  // app.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  // This is only needed in local dev environment remote environment should always use cdn.
   server.get('*.*', express.static(distFolder, {
     maxAge: '1y'
   }));
 
-  // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [
-      { provide: APP_BASE_HREF, useValue: req.baseUrl },
-      { provide: HOST_NAME, useValue: req.headers.host /*'e4cq5a4vfc.execute-api.us-east-1.amazonaws.com'*/ },
-      { provide: PROTOCOL, useValue: 'https' },
-      // { provide: HOST_NAME, useValue: 'localhost:4000' /*'e4cq5a4vfc.execute-api.us-east-1.amazonaws.com'*/ },
-      // { provide: PROTOCOL, useValue: 'http' },
-      { provide: REQUEST, useValue: req }
-    ]});
+  // All regular routes now use the CommonEngine
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+    
+    commonEngine
+      .render({
+        bootstrap: AppServerModule,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: distFolder,
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: HOST_NAME, useValue: headers.host },
+          { provide: PROTOCOL, useValue: 'https' },
+          { provide: REQUEST, useValue: req }
+        ],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
   });
 
   return server;
